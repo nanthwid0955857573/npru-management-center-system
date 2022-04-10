2849f409166611918a718460b376fc247832d4b3
Pb030638 <nanthwid101@gmail.com>
2022-04-10 15:57:58 +0700

    set data TABLE LAYOUT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MenuPageModule } from './modules/menu-page/menu-page.module'
+import {BowrowRecordModule} from './modules/bowrow-record/bowrow-record.module';
 const routes: Routes = [
   // { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LogInComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'menuPage',  loadChildren: () => MenuPageModule}
+  { path: 'menuPage',  loadChildren: () => MenuPageModule},
+  { path: 'readerRecord', loadChildren: () => BowrowRecordModule}
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
